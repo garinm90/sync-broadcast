5923332f50105302f956ed0c2ad83899e06b2666
Max Garin <maxgarin@gmail.com>
2019-01-09 14:31:50 -0500

    Setup so we can bind to a used port

--- broadcaster/broadcast.py
+++ broadcaster/broadcast.py
@@ -3,23 +3,25 @@ import struct
 import serial
 import time
 
+host = ''
+port = 32320
+
 try:
     ser = serial.Serial('/dev/ttyUSB0', 115200)
 except:
     pass
 
 new_line = "\n".encode('utf-8')
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
 
-#sock.bind(('', 32320))
-#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
-#sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('<broadcast>', port))
 
 
 while True:
-    data, address = s.recvfrom(1024)
-    if address == ('192.168.1.143', 0):
-        ser.write(data + new_line)
-        print(data)
+    data = s.recv(1024)
+    # if address == ('192.168.1.143', 47334):
+        # ser.write(data + new_line)
+    print(data)
 
