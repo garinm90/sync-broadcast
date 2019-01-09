7483ed5280604d7051a47ce599443e2fb73dffe2
Max Garin <maxgarin@gmail.com>
2019-01-09 11:07:55 -0500

    Update to recieve data without opening a socket port

--- broadcaster/broadcast.py
+++ broadcaster/broadcast.py
@@ -7,14 +7,16 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 new_line = "\n".encode('utf-8')
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.bind(('', 32320))
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+#sock.bind(('', 32320))
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+#sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
 while True:
-    data  = sock.recv(10240)
-    ser.write(data + new_line)
-    print(data)
+    data, address  = sock.recvfrom_into(10240)
+    if address == '192.168.1.143':
+        print(data)
+    #ser.write(data + new_line)
+    #print(data)
 
