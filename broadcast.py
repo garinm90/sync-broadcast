b76168b948ccec37c59ad365901ac30519b7d537
Max Garin <maxgarin@gmail.com>
2019-01-09 07:01:15 -0500

    fixed udp packet recieving

---
+++ broadcast.py
@@ -0,0 +1,18 @@
+import socket
+import struct
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.bind(('', 32320))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+
+while True:
+    data  = sock.recv(10240)
+    ser.write(data + "\n")
+
